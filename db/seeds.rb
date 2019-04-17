f1dab7bf4f5e684003aba10f5618f776ea516de9
github username <github email address>
2019-04-17 16:55:55 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    Category.delete_all
+    Post.delete_all
+    PostCategory.delete_all
+
+    educational = Category.create(name: "educational")
+    post1 = Post.create(title: "fl", content: "loves teaching")
+    post2 = Post.create(title: "coding", content: "best thing ever")
+
+    pc1 = PostCategory.create(category_id: 1, post: post1)
+    pc2 = PostCategory.create(category_id: 1, post: post2)
